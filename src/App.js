c8bd6c58137db9b30f9802a7ce4f7cc3b4d94710
dangm <dang.m@seznam.cz>
2020-05-05 20:26:09 +0200

    added pages component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </div>
+  );
 }
 
 export default App;
