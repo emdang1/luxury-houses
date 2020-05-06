6c99d07c9a5cfb3d864b805169ea945f14b39ea7
dangm <dang.m@seznam.cz>
2020-05-06 15:02:04 +0200

    added featured rooms component, shown rooms, basic styling

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import RoomProvider, { RoomContext, RoomConsumer } from '../context';
 import Loading from '../components/Loading';
+import Room from './Room';
+import Title from './Title';
 
 class FeaturedRooms extends Component {
   // connecting to the context
@@ -8,8 +10,16 @@ class FeaturedRooms extends Component {
 
   render() {
     const { featuredRooms, loading } = this.context;
+    let rooms = featuredRooms.map((room, i) => <Room key={i} room={room} />);
 
-    return loading ? <Loading /> : 'loaded';
+    return (
+      <section className='featured-rooms'>
+        <Title title='featured rooms' />
+        <div className='featured-rooms-center'>
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
 
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Room = () => {
-  return <div>Hello Room</div>;
+  return <div className='room'>Hello from Room</div>;
 };
 
 export default Room;
