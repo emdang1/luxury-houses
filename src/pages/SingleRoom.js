d841aa3d3eddad4f29b4295e195b49ab5a771f44
dangm <dang.m@seznam.cz>
2020-05-07 10:37:27 +0200

    single room component finished, added all the parts and info

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -46,13 +46,47 @@ class SingleRoom extends Component {
       images,
     } = room;
     return (
-      <StyledHero img={images[0]}>
-        <Banner title={`${name} room`}>
-          <Link to='/rooms' className='btn-primary'>
-            Back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={images[0]}>
+          <Banner title={`${name} room`}>
+            <Link to='/rooms' className='btn-primary'>
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className='single-room'>
+          <div className='single-room-images'>
+            {images.map((imgUrl, i) => (
+              <img key={i} src={imgUrl} alt='room pic' />
+            ))}
+          </div>
+          <div className='single-room-info'>
+            <article>
+              <h3>Details</h3>
+              <p>{description}</p>
+            </article>
+            <article className='info'>
+              <h3>Info</h3>
+              <h6>Price: ${price}</h6>
+              <h6>Size: {size} SQFT</h6>
+              <h6>
+                Capacity:{' '}
+                {capacity > 1 ? `${capacity} People` : `${capacity} person`}
+              </h6>
+              <h6>Pets: {pets ? 'Allowed' : 'Not allowed'}</h6>
+              <h6>{breakfast && 'Free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className='room-extras'>
+          <h6>Extras</h6>
+          <ul className='extras'>
+            {extras.map((item, i) => (
+              <li key={i}>*{item}</li>
+            ))}
+          </ul>
+        </section>
+      </>
     );
   }
 }
